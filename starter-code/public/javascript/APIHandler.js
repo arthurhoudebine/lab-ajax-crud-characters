9256b4fdcd68b58b9a7a96d16b7dfa5dfda77980
Arthur Houdebine <arthur.houdebine@gmail.com>
2019-01-15 21:35:47 +0100

    get route character/:id works

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -2,13 +2,25 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+  
   getFullList () {
-
+  axios.get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);
+      })
+      .catch(err => {
+          console.log('Error is: ', err);
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(responseFromAPI => {
+        console.log('Response from API is: ', responseFromAPI.data);
+    })
+    .catch(err => {
+        console.log('Error is: ', err);
+    })
   }
 
   createOneRegister () {
